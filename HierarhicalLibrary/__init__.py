5cb9e817dcfb2c5036ffc85fe381452e16010a8b
Polushin Maksim <polushinmc@gmail.com>
2022-05-15 15:13:49 +0400

    Some edits

--- HierarhicalLibrary/__init__.py
+++
@@ -1,4 +0,0 @@
-from .CategoryTree import CategoryTree
-from .Classifier import Classifier
-from .Encoders import LdaEncoder, NavecEncoder, FasttextEncoder, BertEncoder
-from .TextProcessor import TextProcessor
